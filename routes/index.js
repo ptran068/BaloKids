a5b85e03251461bdcc03c1b235659249cbda8a05
Phong <ptran068@gmail.com>
2018-11-02 16:33:34 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('admin/cate_list', { title: 'Express' });
+    res.render('admin/user_list', { title: 'Express' });
 });
 
 module.exports = router;
